85be0527bb8d6ba0328a4b2fd9af4767c109304d
Miguel <miguelarrieta02@gmail.com>
2021-07-27 13:29:55 +0100

    Specified namespace with 'using' keyword

--- getting-started/favorite_number.cpp
+++ getting-started/favorite_number.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Enter your favorite number between 1 and 100: ";
-    int favoriteNumber;
-    std::cin >> favoriteNumber;
+using std::cout;
+using std::cin;
+using std::endl;
 
-    std::cout << "Amazing!! That's my favorite number too!" << std::endl;
-    std::cout << "No really!! " << favoriteNumber << " is my favorite number!" << std::endl;
+int main() {
+    cout << "Enter your favorite number between 1 and 100: ";
+    int favoriteNumber;
+    cin >> favoriteNumber;
+
+    cout << "Amazing!! That's my favorite number too!" << endl;
+    cout << "No really!! " << favoriteNumber << " is my favorite number!" << endl;
 
     return 0;
 }
