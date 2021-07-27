1000ebb4688f92fe4128adabde6596f6fbdda4d0
Miguel <miguelarrieta02@gmail.com>
2021-07-27 13:26:58 +0100

    Switched to camelCase

--- getting-started/favorite_number.cpp
+++ getting-started/favorite_number.cpp
@@ -2,11 +2,11 @@
 
 int main() {
     std::cout << "Enter your favorite number between 1 and 100: ";
-    int favorite_number;
-    std::cin >> favorite_number;
+    int favoriteNumber;
+    std::cin >> favoriteNumber;
 
     std::cout << "Amazing!! That's my favorite number too!" << std::endl;
-    std::cout << "No really!! " << favorite_number << " is my favorite number!" << std::endl;
+    std::cout << "No really!! " << favoriteNumber << " is my favorite number!" << std::endl;
 
     return 0;
 }
